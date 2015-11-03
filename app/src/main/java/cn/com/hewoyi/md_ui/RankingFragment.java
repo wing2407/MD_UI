a9735228c618fb4d3f3e09b83e8e583deccc14bd
wing2407 <wing2407@sina.com>
2015-11-03 19:18:08 +0800

    搞定popumenu

--- app/src/main/java/cn/com/hewoyi/md_ui/RankingFragment.java
+++ app/src/main/java/cn/com/hewoyi/md_ui/RankingFragment.java
@@ -3,19 +3,16 @@ package cn.com.hewoyi.md_ui;
 
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.PopupMenu;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.ListPopupWindow;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
-
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,35 +40,33 @@ public class RankingFragment extends Fragment {
         }
     }
 
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ranking, container, false);
 
-        Button btn = (Button) view.findViewById(R.id.fg_ranking_imgbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        final Button button = (Button) view.findViewById(R.id.fg_ranking_imgbtn);
+        final ListPopupWindow mListPop = new ListPopupWindow(getActivity());
+        mListPop.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, rankingType));
+        mListPop.setWidth(ListPopupWindow.WRAP_CONTENT);
+        mListPop.setAnchorView(button);//设置ListPopupWindow的锚点，即关联PopupWindow的显示位置和这个锚点
+        mListPop.setModal(true);//设置是否是模式
+        mListPop.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                button.setText(rankingType[position]);
+                mListPop.dismiss();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(getActivity(), v);
-
-                // This activity implements OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.menu_archive:
-                                Toast.makeText(getActivity(), "archive", Toast.LENGTH_LONG).show();
-                                return true;
-                            case R.id.menu_delete:
-                                Toast.makeText(getActivity(), "delete", Toast.LENGTH_LONG).show();
-                                return true;
-                            default:
-                                return false;
-                        }
-                    }
-                });
-                popup.inflate(R.menu.rankingfragment_button);
-                popup.show();
+                mListPop.show();
             }
         });
 
@@ -103,5 +98,6 @@ public class RankingFragment extends Fragment {
             "Froyo", "Gingerbread", "Honeycomb",
             "Icecream Sandwich", "Jelly Bean", "Kitkat", "Lollipop"};
 
+    public static final String[] rankingType = {"默认排行", "按热度排行", "按下载排行", "按关注排行", "按评分排行", "按评论排行", "周下载排行", "月下载排行", "总下载排行"};
 
 }
